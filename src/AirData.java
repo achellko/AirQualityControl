60a8e33d50c1836d1c6358bb424c9ad629eacdf7
achellko <biara.prime@gmail.com>
2018-05-16 21:22:08 +0200

    upload project on github

---
+++ src/AirData.java
@@ -0,0 +1,184 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class AirData {
+    private static String provinceURL = "http://api.gios.gov.pl/pjp-api/rest/station/findAll";
+    private static String stationURL = "http://api.gios.gov.pl/pjp-api/rest/station/sensors/";
+    private static String airDataURL = "http://api.gios.gov.pl/pjp-api/rest/data/getData/";
+
+    private static JSONArray getJSON(String url) {
+        JSONParser parser = new JSONParser();
+        JSONArray a = null;
+        try {
+            URL oracle = new URL(url); // URL to Parse
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+               a = (JSONArray) parser.parse(inputLine);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return a;
+    }
+
+    private static JSONObject getJSONObject(String url) {
+        JSONParser parser = new JSONParser();
+        JSONObject a = null;
+        try {
+            URL oracle = new URL(url); // URL to Parse
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                a = (JSONObject) parser.parse(inputLine);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return a;
+    }
+
+    private static HashMap<String, List<String>> sortByProvince(HashMap<String,String> stationMap){
+        Set<String> provinceSet = new HashSet<>(stationMap.values());
+        Map<String, List<String>> provinceMap = new HashMap<>();
+
+        for (String s : provinceSet) {
+            List<String> IdList = new ArrayList<>();
+            for (HashMap.Entry<String ,String> entry : stationMap.entrySet()) {
+                if (entry.getValue().equals(s)){
+                    IdList.add(entry.getKey());
+                }
+            }
+            provinceMap.put(s,IdList);
+        }
+        return (HashMap<String, List<String>>) provinceMap;
+    }
+
+    private static Map<String, List<String>> getProvinceMap() {
+        HashMap<String, String> stationMap = new HashMap<>();
+        JSONArray a = getJSON(provinceURL);
+        // Loop through each item
+        for (Object o : a) {
+            JSONObject json = (JSONObject) o;
+            JSONObject city = (JSONObject) json.get("city");
+            JSONObject commune = (JSONObject) city.get("commune");
+
+            stationMap.put(json.get("id").toString(), commune.get("provinceName").toString());
+        }
+        Map<String, List<String>> provinceMap = sortByProvince(stationMap);
+        return provinceMap;
+    }
+
+    private static HashMap<String, String> getDataID(String stationNumber) {
+        String airURL = stationURL+stationNumber;
+        HashMap<String, String> stationAirData = new HashMap<>();
+        JSONArray a = getJSON(airURL);
+        // Loop through each item
+        for (Object o : a) {
+            JSONObject json = (JSONObject) o;
+            JSONObject param = (JSONObject) json.get("param");
+            stationAirData.put(param.get("paramCode").toString(), json.get("id").toString());
+        }
+        return stationAirData;
+    }
+
+    private static String getValueFromURL(String index){
+        airDataURL = airDataURL+index;
+        String result = new String ();
+        int i = 0;
+
+        JSONObject a = getJSONObject(airDataURL);
+
+        HashMap b = (HashMap) ((HashMap)a);
+        String key = (String) b.get("key");
+
+        JSONArray l = (JSONArray) b.get("values");
+        HashMap hm = new HashMap();
+
+        for (Object o : l) {
+            if (i == 1)
+                break;
+            result = (String) ((HashMap)o).get("value").toString();
+            hm.put( key, ((HashMap)o).get("value"));
+            i++;
+        }
+        return result;
+    }
+
+    private static void getData(HashMap index) {
+        HashMap data = new HashMap();
+        for (Object key : index.keySet()) {
+            String keyStr = index.get(key).toString();
+
+            data.put(keyStr, getValueFromURL(keyStr) );
+        }
+    }
+
+    private static void getAirData(HashMap<String, ArrayList> provinceMap){
+        ArrayList<HashMap> listOfMaps = new ArrayList<>();
+        for (Object key : provinceMap.keySet()){
+            Object listOfData = provinceMap.get(key);
+            for (int i = 0; i < ((ArrayList) listOfData).size(); i++) {
+                Object listOfIndex = ((ArrayList)listOfData).get(i);
+                getData((HashMap) listOfIndex);
+                }
+        }
+    }
+
+    private static  HashMap<String, ArrayList> getListOfDataFromProvinces(HashMap map){
+        ArrayList<HashMap> listOfMaps = new ArrayList<>();
+        HashMap<String, ArrayList> provinceStations = new HashMap<>();
+        for (Object key : map.keySet()) {
+            Object listOfStations = map.get(key);
+            for (int i = 0; i < ((ArrayList) listOfStations).size(); i++) {
+                listOfMaps.add(getDataID(((ArrayList) listOfStations).get(i).toString()));
+            }
+            ArrayList<HashMap> tmp = (ArrayList)listOfMaps.clone();
+            provinceStations.put(key.toString(), tmp);
+            listOfMaps.clear();
+        }
+        return provinceStations;
+    }
+
+    private static String sortByData(ArrayList<HashMap> listOfStations) {
+        return "fff";
+    }
+
+    public static void main(String[] args) {
+        HashMap m = new HashMap (getProvinceMap());
+        System.out.println(m.toString());
+        HashMap m2 = new HashMap((getDataID("761")));
+        System.out.println(m2.toString());
+
+        HashMap m3 = getListOfDataFromProvinces(m);
+        System.out.println(m3.toString());
+        getAirData(m3);
+    }
+
+}
